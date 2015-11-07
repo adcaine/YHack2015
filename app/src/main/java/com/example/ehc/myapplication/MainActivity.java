d7e7dc82873fc1918e14f914298ae0ba325eabaf
chenehc <chenehc@users.noreply.github.com>
2015-11-07 13:11:11 -0500

    Add navigation view

--- app/src/main/java/com/example/ehc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ehc/myapplication/MainActivity.java
@@ -4,14 +4,22 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.provider.Settings;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.HeaderViewListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
 
 import com.microsoft.band.BandClient;
 import com.microsoft.band.BandClientManager;
@@ -40,6 +48,8 @@ public class MainActivity extends Activity {
     double elapsedTime;
     private boolean firstPoll = true;
     private ArrayList<Integer> BPMList;
+    private NavigationView mNavigationView;
+    private DrawerLayout mDrawerLayout;
 
     private BandHeartRateEventListener mHeartRateEventListener = new BandHeartRateEventListener() {
         @Override
@@ -71,45 +81,45 @@ public class MainActivity extends Activity {
         }
     };
 
-    private void CreateButton() {
-        try {
-// get the current set of tiles
-            List<BandTile> tiles =
-                    bandClient.getBandTileManager().getTiles().await();
-        } catch (BandException e) {
-// handle BandException
-        } catch (InterruptedException e) {
-
-// handle InterruptedException
-// }
-            // Create the small and tile icons from writable bitmaps.
-// Small icons are 24x24 pixels.
-            Bitmap smallIconBitmap = Bitmap.createBitmap(24, 24, null);
-            BandIcon smallIcon = BandIcon.toBandIcon(smallIconBitmap);
-// Tile icons are 46x46 pixels for Microsoft Band 1 and 48x48 pixels
-// for Microsoft Band 2.
-            Bitmap tileIconBitmap = Bitmap.createBitmap(46, 46, null);
-            BandIcon tileIcon = BandIcon.toBandIcon(tileIconBitmap);
-// create a new UUID for the tile
-            UUID tileUuid = UUID.randomUUID();
-// create a new BandTile using the builder
-// add optional small icon
-// enable badging (the count of unread messages)
-            BandTile tile = new BandTile.Builder(tileUuid, "YHack", tileIcon)
-                    .setTileSmallIcon(smallIcon).setBadgingEnabled(true).build();
-            tile.IsBadingEnabled = true;
-            try {
-                if (bandClient.getBandTileManager().addTile(getActivity(),
-                        tile).await()) {
-// do work if the tile was successfully created
-                }
-            } catch (BandException e) {
-// handle BandException
-            } catch (InterruptedException e) {
-// handle InterruptedException
-            }
-        }
-    }
+//    private void CreateButton() {
+//        try {
+//// get the current set of tiles
+//            List<BandTile> tiles =
+//                    bandClient.getBandTileManager().getTiles().await();
+//        } catch (BandException e) {
+//// handle BandException
+//        } catch (InterruptedException e) {
+//
+//// handle InterruptedException
+//// }
+//            // Create the small and tile icons from writable bitmaps.
+//// Small icons are 24x24 pixels.
+//            Bitmap smallIconBitmap = Bitmap.createBitmap(24, 24, null);
+//            BandIcon smallIcon = BandIcon.toBandIcon(smallIconBitmap);
+//// Tile icons are 46x46 pixels for Microsoft Band 1 and 48x48 pixels
+//// for Microsoft Band 2.
+//            Bitmap tileIconBitmap = Bitmap.createBitmap(46, 46, null);
+//            BandIcon tileIcon = BandIcon.toBandIcon(tileIconBitmap);
+//// create a new UUID for the tile
+//            UUID tileUuid = UUID.randomUUID();
+//// create a new BandTile using the builder
+//// add optional small icon
+//// enable badging (the count of unread messages)
+//            BandTile tile = new BandTile.Builder(tileUuid, "YHack", tileIcon)
+//                    .setTileSmallIcon(smallIcon).setBadgingEnabled(true).build();
+//            tile.IsBadingEnabled = true;
+//            try {
+//                if (bandClient.getBandTileManager().addTile(getActivity(),
+//                        tile).await()) {
+//// do work if the tile was successfully created
+//                }
+//            } catch (BandException e) {
+//// handle BandException
+//            } catch (InterruptedException e) {
+//// handle InterruptedException
+//            }
+//        }
+//    }
 
     //calculates the average of all heart rate stored from the past 30s
     private void calcAvgBPM(List<Integer> list){
@@ -139,11 +149,115 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         txtStatus = (TextView) findViewById(R.id.txtStatus);
+
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //setting up selected item listener
+        mNavigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+
+
+        if (mNavigationView != null) {
+            setupDrawerContent(mNavigationView);
+        }
+
 //        BandInfo[] pairedBands = BandClientManager.getInstance().getPairedBands();
 //        BandClient bandClient = BandClientManager.getInstance().create(getActivity(), pairedBands[0]);
         final WeakReference<Activity> reference = new WeakReference<Activity>(this);
         new HeartRateConsentTask().execute(reference);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+
+        addItemsRunTime(navigationView);
+
+        //setting up selected item listener
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
+    private void addItemsRunTime(NavigationView navigationView) {
+
+        //adding items run time
+        final Menu menu = navigationView.getMenu();
+        for (int i = 1; i <= 3; i++) {
+            menu.add("Runtime item "+ i);
+        }
+
+        // adding a section and items into it
+        final SubMenu subMenu = menu.addSubMenu("SubMenu Title");
+        for (int i = 1; i <= 2; i++) {
+            subMenu.add("SubMenu Item " + i);
+        }
+
+        // refreshing navigation drawer adapter
+        for (int i = 0, count = mNavigationView.getChildCount(); i < count; i++) {
+            final View child = mNavigationView.getChildAt(i);
+            if (child != null && child instanceof ListView) {
+                final ListView menuView = (ListView) child;
+                final HeaderViewListAdapter adapter = (HeaderViewListAdapter) menuView.getAdapter();
+                final BaseAdapter wrapped = (BaseAdapter) adapter.getWrappedAdapter();
+                wrapped.notifyDataSetChanged();
+            }
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        if (isNavDrawerOpen()) {
+            closeNavDrawer();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    protected boolean isNavDrawerOpen() {
+        return mDrawerLayout != null && mDrawerLayout.isDrawerOpen(GravityCompat.START);
+    }
+
+    protected void closeNavDrawer() {
+        if (mDrawerLayout != null) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id) {
+
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
+            case R.id.action_settings:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void panicAction(){
